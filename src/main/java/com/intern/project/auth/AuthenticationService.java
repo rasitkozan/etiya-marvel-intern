c833d280967be2d322dfc50b1b6773f4e4cf489f
Kadir Demirel <Kadirdemirel_17@hotmail.com>
2022-09-08 22:15:14 +0300

    Spring Security and JWT created

--- src/main/java/com/intern/project/auth/AuthenticationService.java
+++ src/main/java/com/intern/project/auth/AuthenticationService.java
@@ -1,4 +1,33 @@
 package com.intern.project.auth;
 
-public class AuthenticationService {
+import com.intern.project.dtos.UserDto;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+@Service
+public class AuthenticationService implements UserDetailsService {
+    //TODO: UserService sisteme enjecte edilecek.
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        UserDetails userDetails = null;
+        //TODO: username ile beraber gidip(userservice) database'e gidip bakılacak varsa oluşturulacak.
+        //TODO: user oluşturulacak.
+        UserDto userDto = new UserDto();
+
+        if (Objects.nonNull(userDto)) {
+            List<GrantedAuthority> grantedAuthorityList = new ArrayList<>();
+            grantedAuthorityList.add(new SimpleGrantedAuthority("ROLE_"+userDto.getGeneralTypeDto().getShortCode().toUpperCase()));
+            userDetails = new User(userDto.getUserName(),userDto.getPassword(),grantedAuthorityList);
+        }
+        return userDetails;
+    }
 }
--- src/main/java/com/intern/project/auth/JwtTokenFilter.java
+++ src/main/java/com/intern/project/auth/JwtTokenFilter.java
@@ -1,4 +1,58 @@
 package com.intern.project.auth;
 
-public class JwtTokenFilter {
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+
+@Component
+public class JwtTokenFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private TokenManager tokenManager;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest httpServletRequest,
+                                    HttpServletResponse httpServletResponse,
+                                    FilterChain filterChain) throws ServletException, IOException {
+
+        /**
+         * "Bearer 123hab2355"
+         */
+        final String authHeader = httpServletRequest.getHeader("Authorization");
+
+        String username = null;
+        String token = null;
+
+
+        if (authHeader != null && authHeader.contains("Bearer")) {
+            token = authHeader.substring(7);
+            try {
+                username = tokenManager.getUsernameToken(token);
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        }
+
+        if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+            if (tokenManager.tokenValidate(token)) {
+                UsernamePasswordAuthenticationToken upassToken =
+                        new UsernamePasswordAuthenticationToken(username, null, new ArrayList<>());
+                upassToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(httpServletRequest));
+                SecurityContextHolder.getContext().setAuthentication(upassToken);
+            }
+        }
+
+        filterChain.doFilter(httpServletRequest, httpServletResponse);
+    }
 }
